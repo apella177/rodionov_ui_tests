928971491baef8481da50fe4c1c7a1ba3df950b0
apella177 <apella177@gmail.com>
2019-10-03 16:02:00 +0300

    Добавление ожиданий

--- src/test/java/com/andersen/rodionov/uilearning/bbc/chrome/MainPageChromeTest2.java
+++ src/test/java/com/andersen/rodionov/uilearning/bbc/chrome/MainPageChromeTest2.java
@@ -1,25 +1,25 @@
 package com.andersen.rodionov.uilearning.bbc.chrome;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class MainPageChromeTest2 {
 
     @Test
-    public void checkSearchInput() {
+    public void checkSearchInput() throws Exception {
 
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.bbc.com");
+        waitElementIsDisplayed(driver.findElement(By.cssSelector("#orb-search-q")));
 
         driver.findElement(By.cssSelector("#orb-search-q")).isDisplayed();
-
         driver.findElement(By.cssSelector("#orb-search-q")).clear();
         driver.findElement(By.cssSelector("#orb-search-q")).click();
         driver.findElement(By.cssSelector("#orb-search-q")).sendKeys("USA");
-
         driver.quit();
     }
 
@@ -33,5 +33,22 @@ public class MainPageChromeTest2 {
         driver.get("https://www.bbc.com");
         driver.findElement(By.cssSelector("#orb-nav-links li:nth-child(2)")).click();
         driver.quit();
+    }
+
+
+    private static void waitElementIsDisplayed (WebElement element) throws Exception {
+        int maxCount = 50;
+        int currentCount = 0;
+        while (!element.isDisplayed() && currentCount < maxCount) {
+          try {
+              Thread.sleep(100);
+          }catch (InterruptedException e){
+              e.printStackTrace();
+          }
+          currentCount ++;
+        }
+        if (currentCount >= maxCount) {
+            throw new Exception("Элемент не найден");
+        }
     }
 }
