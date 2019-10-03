557b80804b269d5eb88f831c07496a5c354f8caa
apella177 <apella177@gmail.com>
2019-10-03 16:56:54 +0300

    Добавление ожиданий 2.0

--- src/test/java/com/andersen/rodionov/uilearning/bbc/chrome/MainPageChromeTest2.java
+++ src/test/java/com/andersen/rodionov/uilearning/bbc/chrome/MainPageChromeTest2.java
@@ -1,4 +1,5 @@
 package com.andersen.rodionov.uilearning.bbc.chrome;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,7 +15,9 @@ public class MainPageChromeTest2 {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.bbc.com");
+
         waitElementIsDisplayed(driver.findElement(By.cssSelector("#orb-search-q")));
+        waitElementIsEnabled(driver.findElement(By.cssSelector("#orb-search-q")));
 
         driver.findElement(By.cssSelector("#orb-search-q")).isDisplayed();
         driver.findElement(By.cssSelector("#orb-search-q")).clear();
@@ -26,29 +29,44 @@ public class MainPageChromeTest2 {
     @Test
     public void clickNewsButton() {
 
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+        System.setProperty ("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-
         driver.get("https://www.bbc.com");
+
         driver.findElement(By.cssSelector("#orb-nav-links li:nth-child(2)")).click();
         driver.quit();
     }
 
-
-    private static void waitElementIsDisplayed (WebElement element) throws Exception {
+    private static void waitElementIsDisplayed(WebElement element) throws Exception {
         int maxCount = 50;
         int currentCount = 0;
         while (!element.isDisplayed() && currentCount < maxCount) {
-          try {
-              Thread.sleep(100);
-          }catch (InterruptedException e){
-              e.printStackTrace();
-          }
-          currentCount ++;
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            currentCount++;
         }
         if (currentCount >= maxCount) {
             throw new Exception("Элемент не найден");
+        }
+    }
+
+    private static void waitElementIsEnabled(WebElement element) throws Exception {
+        int maxCount = 50;
+        int currentCount = 0;
+        while (!element.isEnabled() && currentCount < maxCount) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            currentCount++;
+        }
+        if (currentCount >= maxCount) {
+            throw new Exception("Элемент не доступен");
         }
     }
 }
