cf570ff5d1f1df4e517e6f8b40aadb4630c46ecd
apella177 <apella177@gmail.com>
2019-10-10 12:40:46 +0300

    Assertions 1.0

---
+++ src/test/java/com/andersen/rodionov/uilearning/bbc/chrome/AssertionsTests.java
@@ -0,0 +1,42 @@
+package com.andersen.rodionov.uilearning.bbc.chrome;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AssertionsTests {
+
+    @Test
+    public void testAssertNumbers() {
+        int x = 5;
+        int y = 5;
+        Assert.assertEquals(x, y, "Значение X != Значению Y");
+    }
+
+    @Test
+    public void testAssertStringFull() {
+        String x = "abcde";
+        String y = "abcde";
+        Assert.assertEquals(x, y, "Значение X != Значению Y");
+    }
+
+    @Test
+    public void testAssertStringPart() {
+        String x = "abcde";
+        String y = "ab";
+        Assert.assertTrue(x.contains(y), "Значение X не содержит Значение Y");
+    }
+
+    @Test
+    public void testAssertArray() {
+        Integer[] array1 = {12, 34, 56, 77};
+        Integer[] array2 = {77, 56, 34, 12};
+        Assert.assertEqualsNoOrder(array1, array2, "Значения массивов не совпадают");
+    }
+
+    @Test
+    public void testAssertArrayStrings() {
+        String[] array1 = {"qwe", "rty", "uio"};
+        String[] array2 = {"qwe", "rty", "uio"};
+        Assert.assertEquals(array1, array2, "Значения массивов не совпадают");
+    }
+}
